b57fa76a19aab61b4684de7d73cbaf8e96116b4a
Ritesh kumar <sidharthrk7890@gmail.com>
2026-02-16 23:09:01 +0530

    ATM and Number gessing project

---
+++ ARRAYS/18_probleam.c
@@ -0,0 +1,18 @@
+
+
+int main(){
+    int aadhar[5];
+
+    //Taking input 
+
+    int *ptr =  &aadhar[0];
+    for(int i=0; i<5; i++){
+        printf("%d index : ", i);
+        scanf("%d", (ptr+i));
+    }
+    // output
+    for(int i=0; i<5; i++){
+        printf("%d index = %d\n", i , *(ptr+i));
+    }
+    return 0;
+}
